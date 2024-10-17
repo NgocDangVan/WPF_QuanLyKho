547b0c2d843c205d921794126450a5137e3bbe91
NgocDangVan <ngocdangvan98@gmail.com>
2024-10-17 10:52:47 +0700

    Add login screen

---
+++ QuanLyKho/ViewModel/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace QuanLyKho.ViewModel
+{
+    public class MainViewModel: BaseViewModel
+    {
+        public bool IsLoaded { get; set; } = false;
+        public MainViewModel() {
+            if (!IsLoaded)
+            {
+                IsLoaded = true;
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.ShowDialog();
+            }
+        }
+    }
+}
